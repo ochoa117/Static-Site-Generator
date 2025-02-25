68491f4ea691120a48eadef6b8ccfa5e9f12e699
ochoa117 <williamochoa96@gmail.com>
2025-02-25 12:58:48 -0800

    Add text and code files along with a gitignore

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    obj = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+    obj_str = obj.__repr__()
+    print(obj_str)
+
+main()
