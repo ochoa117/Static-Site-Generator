61044530211e047fc563da4cb7e8e96338a1eb39
ochoa117 <williamochoa96@gmail.com>
2025-05-07 11:44:44 -0700

    make static images files an content markdown files, make recursive page generator function and update main

--- src/main.py
+++ src/main.py
@@ -1,21 +1,19 @@
 from textnode import TextNode, TextType
 from markdown_blocks import markdown_to_html_node, extract_title
 import os, shutil
+from pathlib import Path
 
 def main():
-    obj = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
-    obj_str = obj.__repr__()
-    print(obj_str)
 
     static_to_public_copy(
         "/home/minavina96/workspace/github.com/ochoa117/Static-Site-Generator/static",
         "/home/minavina96/workspace/github.com/ochoa117/Static-Site-Generator/public"
     )
 
-    generate_path(
-        "/home/minavina96/workspace/github.com/ochoa117/Static-Site-Generator/content/index.md",
+    generate_pages_recursive(
+        "/home/minavina96/workspace/github.com/ochoa117/Static-Site-Generator/content",
         "/home/minavina96/workspace/github.com/ochoa117/Static-Site-Generator/template.html",
-        "/home/minavina96/workspace/github.com/ochoa117/Static-Site-Generator/public/index.html"
+        "/home/minavina96/workspace/github.com/ochoa117/Static-Site-Generator/public"
     )
 
     
@@ -49,7 +47,6 @@ def generate_path(from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     file_content = ""
     temp_content = ""
-    dest_content = ""
     # Automatically closes files
     with open(from_path) as file:
         file_content = file.read()
@@ -62,5 +59,16 @@ def generate_path(from_path, template_path, dest_path):
         os.makedirs(os.path.dirname(dest_path))
     with open(dest_path, "w") as dest:
         dest.write(new_temp_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    contents = os.listdir(dir_path_content)
+    new_dest_file = Path("index.html")
+    for content in contents:
+        content_path = os.path.join(dir_path_content, content)
+        if not os.path.isfile(content_path):
+            generate_pages_recursive(content_path, template_path, os.path.join(dest_dir_path, content))
+        else:
+            new_dest_dir_path = os.path.join(dest_dir_path, new_dest_file)
+            generate_path(content_path, template_path, new_dest_dir_path)
 
 main()
